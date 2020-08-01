c25deef91a991ca94820e6cf66af059211a72f18
Phạm Quang Hữu <phamhuu.a2.zip@gmail.com>
2020-08-01 20:11:09 +0700

    plus ultra

--- src/index.js
+++ src/index.js
@@ -4,20 +4,7 @@ const { request, response } = require('express');
 const { createPool } = require('mysql');
 const app = express();
 
-
-
-
-
-// const hamxuly = (request, response) => {
-
-//     const data = {
-//         name: 'huu',
-//         tuoi: '18'
-//     } 
-//     response.send(data);
-// }
-
-app.get('/', (request, response) => {
+app.get('/user', (request, response) => {
 
     pool.query('select * from `user`;', (err, data) => {
         if (err) { response.send(err) }
@@ -25,6 +12,35 @@ app.get('/', (request, response) => {
         response.send(data)
     })
 });
+
+app.get('/category', (request, response) => {
+
+    pool.query('select * from `category`;', (err, data) => {
+        if (err) { response.send(err) }
+        console.log(data);
+        response.send(data)
+    })
+});
+
+app.get('/order', (request, response) => {
+
+    pool.query('select * from `order`;', (err, data) => {
+        if (err) { response.send(err) }
+        console.log(data);
+        response.send(data)
+    })
+});
+
+app.get('/product', (request, response) => {
+
+    pool.query('select * from `product`;', (err, data) => {
+        if (err) { response.send(err) }
+        console.log(data);
+        response.send(data)
+    })
+});
+
+
 const callback = () => {
     console.log("mày đã chạy ở cổng 8080");
 }
